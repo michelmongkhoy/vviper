1aca45e94ed20ba06e9a440a49676ec4863ca2d7
Michel Mongkhoy <mmongkhoy@viadeoteam.com>
2017-07-10 15:44:08 +0200

    Add Interactor file

---
+++ Interactor/ContactListInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  ContactListInteractor.swift
+//  vviper
+//
+//  Created by mmongkhoy on 10/07/2017.
+//  Copyright © 2017 Viadeo. All rights reserved.
+//
+
+import Foundation
+
+struct ContactListInteractor {
+
+}
