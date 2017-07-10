7cc39671c4f1023282db30df487b150082167e30
Michel Mongkhoy <mmongkhoy@viadeoteam.com>
2017-07-10 15:20:14 +0200

    Show ContactListVC

--- vviper/AppDelegate.swift
+++ vviper/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        let storyboard = UIStoryboard.init(name: "ContactListStoryboard", bundle: nil)
+        let vc = storyboard.instantiateInitialViewController()
+
+        self.window?.rootViewController = vc
+
         return true
     }
 
