5dd8a5cc606dec7fe1d7ad7845de09d1047e8f4c
Xavier Maillot <xmaillot@viadeoteam.com>
2017-07-12 15:20:10 +0200

    Create file tree for Home2

---
+++ vviper/Modules/Home2/Home2Interactor.swift
@@ -0,0 +1,10 @@
+
+//
+//  Home2Interactor.swift
+//  vviper
+//
+//  Created by Xavier Maillot on 12/07/2017.
+//  Copyright © 2017 Viadeo. All rights reserved.
+//
+
+import Foundation
---
+++ vviper/Modules/Home2/Home2Presenter.swift
@@ -0,0 +1,9 @@
+//
+//  Home2Presenter.swift
+//  vviper
+//
+//  Created by Xavier Maillot on 12/07/2017.
+//  Copyright © 2017 Viadeo. All rights reserved.
+//
+
+import Foundation
